08f16e2e57af54125ab867169cb6b05efd3a06be
Caroline Chan <carolinechan304@gmail.com>
2022-09-23 14:21:14 +0700

    Edit table view cell's view to show hero's name

--- Funios-Board/Funios-Board/Presentation/Dota/DotaHeroTableViewCell/DotaHeroTableViewCell.swift
+++ Funios-Board/Funios-Board/Presentation/Dota/DotaHeroTableViewCell/DotaHeroTableViewCell.swift
@@ -8,16 +8,18 @@
 import UIKit
 
 class DotaHeroTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var heroName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
+    func setupData(heroName: String) {
+        self.heroName.text = heroName
+    }
 }
