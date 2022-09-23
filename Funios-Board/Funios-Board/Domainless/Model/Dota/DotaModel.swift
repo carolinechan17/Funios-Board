3c5e0ceae85470891ce5e85facdeee9b06b390fd
Caroline Chan <carolinechan304@gmail.com>
2022-09-23 14:05:57 +0700

    Create dota model

---
+++ Funios-Board/Funios-Board/Domainless/Model/Dota/DotaModel.swift
@@ -0,0 +1,27 @@
+//
+//  DotaModel.swift
+//  Funios-Board
+//
+//  Created by Caroline Chan on 23/09/22.
+//
+import Foundation
+
+// MARK: - DotaModelElement
+struct DotaModelElement: Codable {
+    let id: Int
+    let name, localizedName: String
+    let primaryAttr: String
+    let attackType: String
+    let roles: [String]
+    let legs: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case localizedName = "localized_name"
+        case primaryAttr = "primary_attr"
+        case attackType = "attack_type"
+        case roles, legs
+    }
+}
+
+typealias DotaModel = [DotaModelElement]
