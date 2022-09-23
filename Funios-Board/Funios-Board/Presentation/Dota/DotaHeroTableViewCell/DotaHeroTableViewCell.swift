c64c814fe60629a903165ff93dbddb3f46d6249b
Caroline Chan <carolinechan304@gmail.com>
2022-09-23 14:14:55 +0700

    Add table view cell to show list of hero

---
+++ Funios-Board/Funios-Board/Presentation/Dota/DotaHeroTableViewCell/DotaHeroTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DotaHeroTableViewCell.swift
+//  Funios-Board
+//
+//  Created by Caroline Chan on 23/09/22.
+//
+
+import UIKit
+
+class DotaHeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
